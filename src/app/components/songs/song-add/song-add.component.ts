b9a24f2b9f2d857811b05ad0f96e21e5d30d3a58
LG <luis.gomez.comin@gmail.com>
2025-03-10 15:26:59 +0100

    Initial setup: structure, mock.json, and first components

---
+++ src/app/components/songs/song-add/song-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-add',
+  imports: [],
+  templateUrl: './song-add.component.html',
+  styleUrl: './song-add.component.css'
+})
+export class SongAddComponent {
+
+}
