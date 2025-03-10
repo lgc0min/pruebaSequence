aec63b2a18cdba7e2d67404e8078c4c9a8b8bc26
LG <luis.gomez.comin@gmail.com>
2025-03-10 18:55:30 +0100

    Configure HTTP provider and adjust routing settings.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SongListComponent } from './components/songs/song-list/song-list.compon
 import { SongDetailComponent } from './components/songs/song-detail/song-detail.component';
 import { SongAddComponent } from './components/songs/song-add/song-add.component';
 
-export const routes: Routes = [
+export const appRoutes: Routes = [
     { path: 'songs', component: SongListComponent },
     { path: 'song/:id', component: SongDetailComponent },
     { path: 'song-add', component: SongAddComponent },
@@ -12,7 +12,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule],
 })
 export class AppRoutingModule { }
