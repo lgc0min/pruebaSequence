6dba083f89037c8077afe1e5f961bd58112184f9
LG <luis.gomez.comin@gmail.com>
2025-03-10 20:35:05 +0100

    Updated app.routes.ts to initialize from the starting point of the main branch.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SongListComponent } from './components/songs/song-list/song-list.component';
-import { SongDetailComponent } from './components/songs/song-detail/song-detail.component';
-import { SongAddComponent } from './components/songs/song-add/song-add.component';
 
 export const appRoutes: Routes = [
-    { path: 'songs', component: SongListComponent },
-    { path: 'song/:id', component: SongDetailComponent },
-    { path: 'song-add', component: SongAddComponent },
-    { path: '', redirectTo: '/songs', pathMatch: 'full' },
+    
 ];
 
 @NgModule({
