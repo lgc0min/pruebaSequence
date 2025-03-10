29690ab77d67c2ec9a66595988429a2f5a9a0ac8
LG <luis.gomez.comin@gmail.com>
2025-03-10 22:19:07 +0100

    Update routes for songs component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SongDetailComponent } from './components/songs/song-detail/song-detail.component';
+import { SongListComponent } from './components/songs/song-list/song-list.component';
+import { SongAddComponent } from './components/songs/song-add/song-add.component';
 
 export const appRoutes: Routes = [
-    
+    { path: '', redirectTo: '/songs', pathMatch: 'full' },
+    { path: 'songs', component: SongListComponent },
+    { path: 'songs/add', component: SongAddComponent },
+    { path: 'songs/:id', component: SongDetailComponent },
 ];
 
 @NgModule({
