d08373a67df87ce2b949b8f1e40d04263f32cc0b
LG <luis.gomez.comin@gmail.com>
2025-03-10 15:58:08 +0100

    Configure navigation routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SongListComponent } from './components/songs/song-list/song-list.component';
+import { SongDetailComponent } from './components/songs/song-detail/song-detail.component';
+import { SongAddComponent } from './components/songs/song-add/song-add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'songs', component: SongListComponent },
+    { path: 'song/:id', component: SongDetailComponent },
+    { path: 'song-add', component: SongAddComponent },
+    { path: '', redirectTo: '/songs', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
